e3bfdd2bf421354af57a82c269eb0b36f3fc4170
Michael Bowman <bowmanmc@gmail.com>
2017-05-01 21:29:12 -0400

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return (
+        <div className="HomePage">
+            <h1>QuickBits Administration</h1>
+        </div>
+    );
+};
+
+export default HomePage;
